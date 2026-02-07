ddd327b07bb560a8fe187aabbeb863426baedcfb
ratanajaya <ratanajayaa@gmail.com>
2026-02-07 11:36:13 +0700

    refactor: create Modal component to replace antd Modal; remove unused stuff in useInputPanel

--- app/book/_components/useInputPanel.tsx
+++ app/book/_components/useInputPanel.tsx
@@ -1,39 +1,26 @@
 import { Textarea } from "@/components/Textarea";
-import { DownOutlined, UpOutlined } from "@ant-design/icons";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { Panel } from "react-resizable-panels";
 
 export default function useInputPanel(props:{
   inputTag: string;
-}){  
-  const [collapseInput1, setCollapseInput1] = useState(false);
-
+}){
   // Use refs instead of state to avoid re-renders
-  const input2Ref = useRef<HTMLTextAreaElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
   // Function to get current values from refs
   const getUserInput = () => ({
-    input1: input2Ref.current?.value || '',
+    input1: inputRef.current?.value || '',
   });
   
   const element = (
-    <>
-      <div className=' w-full flex items-center justify-center mb-1'>
-        <div 
-          className=' h-3 w-32 bg-neutral-700 hover:bg-neutral-600 flex items-center justify-center cursor-pointer rounded-sm'
-          onClick={() => setCollapseInput1(prev => !prev)}
-        >
-          {collapseInput1 ? <UpOutlined className='text-gray-300' /> : <DownOutlined className='text-gray-300' />}
-        </div>
-      </div>
-      <Panel defaultSize={10} minSize={5} order={3}>
-        <Textarea
-          className=' h-full'
-          placeholder={props.inputTag}
-          ref={input2Ref}
-        />
-      </Panel>
-    </>
+    <Panel defaultSize={10} minSize={5} order={3}>
+      <Textarea
+        className=' h-full'
+        placeholder={props.inputTag}
+        ref={inputRef}
+      />
+    </Panel>
   );
 
   return { element, getUserInput };
